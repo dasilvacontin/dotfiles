a992c448c53ba2b137f4dec0228fb82dbf0bb2ac
David da Silva <dasilvacontin@gmail.com>
2016-09-29 13:14:57 +0200

    Make Mac bootstrapper install `notify` script

--- macOS_bootstrap.sh
+++ macOS_bootstrap.sh
@@ -26,6 +26,10 @@ git clone https://github.com/tarjoilija/zgen.git ~/github/tarjoilija/zgen
 # set zsh as default shell
 chsh -s /bin/zsh
 
+# install notify script from gist
+echo "Installing 'notify' script.."
+sudo sh -c "curl https://gist.githubusercontent.com/WengerK/ad0c1330966522c05f504d71d29f675b/raw/34e6e53eb2f5c2039f604c10d3bf272b251b31a5/notify.sh -o /usr/local/bin/notify && chmod +x /usr/local/bin/notify"
+
 # install fzf shell extensions
 /usr/local/opt/fzf/install
 
